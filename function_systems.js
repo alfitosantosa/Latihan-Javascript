8e9de37844a171937b50016869304da688430148
alfitosantosa <44111010166@student.mercubuana.ac.id>
2023-09-28 21:13:18 +0700

    tambah materi function

---
+++ function_systems.js
@@ -0,0 +1,24 @@
+// Javascript
+
+// Function Systems
+
+const getnama = function () {
+  console.log("Hello World");
+};
+
+const getnama2 = () => {
+  console.log("Hello World");
+};
+
+getnama2();
+
+// return
+
+function hitung(a, b) {
+  return a + b;
+}
+
+let hasil = hitung(1, 2);
+console.log(hasil * 10);
+
+//returun itu hasilnya bisa kita lanjutkan lagi perhitunganya..
